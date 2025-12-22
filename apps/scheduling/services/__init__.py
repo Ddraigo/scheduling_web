2402a2944e746f4fadb8b4719ab6354e0739fca3
Ddraigo <laithanh1000@gmai.com>
2025-12-22 17:56:34 +0700

    update chatbot

--- apps/scheduling/services/__init__.py
+++ apps/scheduling/services/__init__.py
@@ -2,17 +2,11 @@
 Services layer for scheduling business logic
 """
 
-from .schedule_validator import ScheduleValidator
-from .batch_scheduler import BatchScheduler
-from .query_handler import QueryHandler
 from .data_access_layer import DataAccessLayer, get_giang_vien_info_dict, get_lop_info_dict
 from .llm_service import LLMDataProcessor, LLMPromptBuilder, get_dataset_json, get_conflict_report_json
-from .schedule_generator_llm import ScheduleGeneratorLLM
+from .chatbot_service import ScheduleChatbot, get_chatbot
 
 __all__ = [
-    'ScheduleValidator',
-    'BatchScheduler',
-    'QueryHandler',
     'DataAccessLayer',
     'get_giang_vien_info_dict',
     'get_lop_info_dict',
@@ -20,7 +14,8 @@ __all__ = [
     'LLMPromptBuilder',
     'get_dataset_json',
     'get_conflict_report_json',
-    'ScheduleGeneratorLLM',
+    'ScheduleChatbot',
+    'get_chatbot',
 ]
 
 
